9aa33d3b9e5f94440461922fea881bf81251d589
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-09-18 16:46:15 -0500

    checking function working

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,8 @@ const character = [
         question: 'What was the name of the diplomatic war between USA and USSR?',
         answer1: 'Cold War',
         answer2: 'World War',
-        answer3: 'American Revolutionary War'
+        answer3: 'American Revolutionary War',
+        correctAnswer: 'Cold War'
     },
     {
         name: 'science',
@@ -15,47 +16,53 @@ const character = [
         question: 'Which of these is NOT a subatomic particle?',
         answer1: 'Leuctron',
         answer2: 'Electron',
-        answer3: 'Neutron'
+        answer3: 'Neutron',
+        correctAnswer: 'Leuctron'
     },
     {
         name: 'geography',
         url: 'assets/img/geography1.png',
         question: 'Nova Scotia is part of which country?',
-        answer1: 'Canada',
+        answer1: 'United States of America',
         answer2: 'United Kingdom',
-        answer3: 'United States of America'
+        answer3: 'Canada',
+        correctAnswer: 'Canada'
     },
     {
         name: 'king',
         url: 'assets/img/king.png',
         question: 'Who is Bruce Wayne?',
-        answer1: 'Batman',
-        answer2: 'Superman',
-        answer3: 'Green Lantern'
+        answer1: 'Superman',
+        answer2: 'Batman',
+        answer3: 'Green Lantern',
+        correctAnswer: 'Batman'
     },
     {
         name: 'enterteiment',
         url: 'assets/img/enterteiment.png',
         question: 'What is the name of Mickey Mouse pet?',
-        answer1: 'Pluto',
+        answer1: 'Donald',
         answer2: 'Goofy',
-        answer3: 'Donald'
+        answer3: 'Pluto',
+        correctAnswer: 'Pluto'
     },
     {
         name: 'art',
         url: 'assets/img/art1.png',
         question: 'Where is the painting the "Mona Lisa" displayed?',
-        answer1: 'Louvre Museum',
-        answer2: 'The Museum Of Modern Art',
-        answer3: 'Vatican Museums'
+        answer1: 'Vatican Museums',
+        answer2: 'Louvre Museum',
+        answer3: 'The Museum Of Modern Art',
+        correctAnswer: 'Louvre Museum'
     },
     {
         name: 'sports',
         url: 'assets/img/sport1.png',
         question: 'Who won Best Player at the 2014 FIFA World Cup held in Brazil?',
-        answer1: 'Lionel Messi',
-        answer2: 'Cristiano Ronaldo',
-        answer3: 'Thomas Müller'
+        answer1: 'Cristiano Ronaldo',
+        answer2: 'Lionel Messi',
+        answer3: 'Thomas Müller',
+        correctAnswer: 'Lionel Messi',
     }        
 ];
 
@@ -98,24 +105,37 @@ const app = {
             $('.spinner').on('click', app.clickHandler);
             $('.spinner span').show();
             app.showQuestion(character);
-            console.log(character.question + ' : '+ character.answer1);
+            console.log(character.question + ' : '+ character.correctAnswer);
             $('#content').removeAttr('hidden');
         });
     },
 
     saveUserAnswer : function (event) {
-        console.log(event);
         app.answers.push({
             question: event.parentNode.firstChild.textContent,
             answer: event.textContent            
         });
-        
+
+        app.correctAnswer();
         $('#content').empty();
         console.log(app.answers);
     },
 
     correctAnswer : function(){
-        
+        let length = app.answers.length - 1;
+        $.each(character, function (index, value) {
+            $.each(app.answers, function(i, v){
+                if(app.answers[length].question == value.question){
+                    if(app.answers[length].answer == value.correctAnswer){
+                        swal('Congrats!','Correct Answer','success');
+                        return true;
+                    }else{
+                        swal('Ooops! Incorrect Answer','Keep trying','error');
+                        return false;
+                    }
+                }
+            });
+        });
     }
 
 }
