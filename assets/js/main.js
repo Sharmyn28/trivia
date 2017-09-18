dd983d9838f5c3aa29027e78cdd2dc87eb036ecc
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-09-17 23:28:11 -0500

    showing questions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,62 @@
+'use strict';
 
 const character = [
     {
         name: 'history',
         url: 'assets/img/history.png',
-        question: undefined,
-        answer: undefined,
+        question: 'What was the name of the diplomatic war between USA and USSR?',
+        answer1: 'Cold War',
+        answer2: 'World War',
+        answer3: 'American Revolutionary War'
     },
     {
         name: 'science',
         url: 'assets/img/science.png',
-        question: undefined,
-        answer: undefined,
+        question: 'Which of these is NOT a subatomic particle?',
+        answer1: 'Leuctron',
+        answer2: 'Electron',
+        answer3: 'Neutron'
     },
     {
         name: 'geography',
         url: 'assets/img/geography.png',
-        question: 'Holaaa',
-        answer: undefined,
+        question: 'Nova Scotia is part of which country?',
+        answer1: 'Canada',
+        answer2: 'United Kingdom',
+        answer3: 'United States of America'
     },
     {
         name: 'king',
-        url: 'assets/img/geography.png',
-        question: 'Holaaa',
-        answer: undefined,
+        url: 'assets/img/king.png',
+        question: 'Who is Bruce Wayne?',
+        answer1: 'Batman',
+        answer2: 'Superman',
+        answer3: 'Green Lantern'
     },
     {
         name: 'enterteiment',
         url: 'assets/img/enterteiment.png',
-        question: undefined,
-        answer: undefined,
+        question: 'What is the name of Mickey Mouse pet?',
+        answer1: 'Pluto',
+        answer2: 'Goofy',
+        answer3: 'Donald'
     },
     {
         name: 'art',
         url: 'assets/img/art.png',
-        question: undefined,
-        answer: undefined,
+        question: 'Where is the painting the "Mona Lisa" displayed?',
+        answer1: 'Louvre Museum',
+        answer2: 'The Museum Of Modern Art',
+        answer3: 'Vatican Museums'
     },
     {
         name: 'sports',
         url: 'assets/img/sports.png',
-        question: undefined,
-        answer: undefined,
-    }       
-    
+        question: 'Who won Best Player at the 2014 FIFA World Cup held in Brazil?',
+        answer1: 'Lionel Messi',
+        answer2: 'Cristiano Ronaldo',
+        answer3: 'Thomas Müller'
+    }        
 ];
 
 const app = {
@@ -59,24 +73,36 @@ const app = {
 
     changeCoverPage : function() {
         setTimeout(function(){
-            $('#coverPage').addClass("invisible");
+            //$('#coverPage').addClass("invisible");
             //$('#coverPage').attr('hidden');
-            $('#content').removeAttr('hidden');
+            //$('#content').removeAttr('hidden');
+            $('#roulette').removeAttr('hidden');
         },2500);
     },
 
-    nextQuestion: function() {
-
+    showQuestion: function(character) {
+        let question = `<h5 id="question">'${character.question}'</h5>`
+        let answer1 = `<button id='${character.answer1}'>${character.answer1}</button>`
+        let answer2 = `<button id='${character.answer2}'>${character.answer2}</button>`
+        let answer3 = `<button id='${character.answer3}'>${character.answer3}</button>`
+        $('#question_div').append(question, answer1, answer2, answer3); 
     },
     
     clickHandler: function() {
         $('.spinner').off('click');
         $('.spinner span').hide();
         $r.spin().done(function(character) {
-            $('#content').text('You choose: ' + character.name);
             console.log(character.name);
             $('.spinner').on('click', app.clickHandler);
             $('.spinner span').show();
+            app.showQuestion(character);
+            /*let question = `<h5 id="question">'${character.question}'</h5>`
+            let answer1 = `<button id='${character.answer1}'>${character.answer1}</button>`
+            let answer2 = `<button id='${character.answer2}'>${character.answer2}</button>`
+            let answer3 = `<button id='${character.answer3}'>${character.answer3}</button>`
+            $('#question_div').append(question, answer1, answer2, answer3);*/
+            console.log(character.question + ' : '+ character.answer1);
+            $('#content').removeAttr('hidden');
         });
     }
 }
